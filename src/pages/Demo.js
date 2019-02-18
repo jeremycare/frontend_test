9cbde5caa82a11f4a501a3d65c408d533458bd5d
care_j <jeremy.care@epitech.eu>
2019-02-18 10:41:48 +0100

    feat(Demo): add state management

--- src/pages/Demo.js
+++ src/pages/Demo.js
@@ -1,16 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 import { getPopup } from '../lib/popup'
 
 import './Demo.css'
 
 export class Demo extends Component {
+  constructor() {
+    super()
+    this.state = {
+      hidden: false,
+    }
+  }
+
+  hidePopup() {
+    this.setState({ hidden: false })
+  }
+
   render() {
     const popup = getPopup()
 
     /**
      * Step 1: Render popup message in an overlay
      */
-    return <div />
+    console.log(popup)
+    return (
+      <div className="Demo">
+        {!this.state.hidden ? (
+          <div className="Overlay">
+            <span>{popup.message}</span>
+          </div>
+        ) : (
+          ''
+        )}
+      </div>
+    )
   }
 }
