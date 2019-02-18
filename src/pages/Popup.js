d104e2e31ed7883508c7b327dac17d26d488eb6b
care_j <jeremy.care@epitech.eu>
2019-02-18 11:57:21 +0100

    feat(Popup/form): add form

--- src/pages/Popup.js
+++ src/pages/Popup.js
@@ -6,7 +6,30 @@ export class Popup extends Component {
   /**
    * Step 3: Render a form and everything needed to be able to edit the popup
    */
+  constructor() {
+    super()
+    this.state = {
+      message: '',
+    }
+    this.handleChangeMessage = this.handleChangeMessage.bind(this)
+  }
+
+  handleChangeMessage(e) {
+    this.setState({
+      message: e.target.value,
+    })
+  }
+
   render() {
-    return <div/>
+    return (
+      <div>
+        <span>New Popup Message :</span>
+        <input
+          value={this.state.message}
+          placeholder="Popup Message"
+          onChange={this.handleChangeMessage}
+        />
+      </div>
+    )
   }
 }
